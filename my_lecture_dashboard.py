daca600c9bc3157d75c3c6d37352d5001b64c1e9
Adebimpe John E. Omolola <lamideadebimpe@gmail.com>
2025-10-21 14:42:18 +0100

    Update my_lecture_dashboard.py

--- my_lecture_dashboard.py
+++ my_lecture_dashboard.py
@@ -168,10 +168,10 @@ mode = st.radio("Select Mode:", ["Student", "Teacher/Admin"])
 if mode=="Student":
     st.subheader("🎓 Student Login & Attendance")
     with st.form("attendance_form"):
-    name = st.text_input("Full Name")
-    matric = st.text_input("Matric Number")
-    week = st.selectbox("Select Lecture Week", lectures_df["Week"].tolist())
-    submit_attendance = st.form_submit_button("Mark Attendance")
+        name = st.text_input("Full Name")
+        matric = st.text_input("Matric Number")
+        week = st.selectbox("Select Lecture Week", lectures_df["Week"].tolist())
+        submit_attendance = st.form_submit_button("Mark Attendance")
 
     if submit_attendance and name.strip() and matric.strip():
         marked = mark_attendance(name, matric, week)
@@ -185,7 +185,7 @@ if mode=="Student":
         else:
             COURSE_CODE = {
                 "BIO203": {"valid_code": "BIO203-ZT7", "start": "13:00", "end": "22:00"},
-                "BCH201": {"valid_code": "BCH201-ZT8", "start": "14:00", "end": "14:45"},
+                "BCH201": {"valid_code": "BCH201-ZT8", "start": "14:00", "end": "14:47"},
                 "MCB221": {"valid_code": "MCB221-ZT9", "start": "10:00", "end": "22:20"},
                 "BIO113": {"valid_code": "BIO113-ZT1", "start": "09:00", "end": "22:00"},
                 "BIO306": {"valid_code": "BIO306-ZT2", "start": "14:00", "end": "22:00"},
@@ -364,6 +364,7 @@ if mode=="Teacher/Admin":
             else: st.info(f"No {label.lower()} yet.")
     else:
         if password: st.error("❌ Incorrect password. Try again.")
+
 
 
 
